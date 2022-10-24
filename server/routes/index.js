7f9f360430e338ca21f796932835c06fafbb02c6
demosthenesd <demosthenesd@icloud.com>
2022-10-24 19:45:57 +1000

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,11 @@ var express = require("express");
 const controllers = require("../controllers");
 var router = express.Router();
 
+const cors = require('cors');
+app.use(cors({
+    origin: 'http://ec2-3-83-108-89.compute-1.amazonaws.com:3000/'
+}));
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
