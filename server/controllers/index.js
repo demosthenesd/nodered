550f3328e2b6b620cd1afcb5776a6a06a3526b30
Aaron Nolan <N11021276@qut.edu.au>
2022-10-24 15:01:50 +1000

    Added test controller

---
+++ server/controllers/index.js
@@ -0,0 +1,14 @@
+const models = require("..database/models");
+
+const getAllLocations = async (req, res) => {
+  try {
+    const locations = await models.location.findAll();
+    return res.status(200).json({ locations });
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+};
+
+module.exports = {
+  getAllLocations,
+};
