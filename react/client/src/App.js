de8f3126a85b3013450abf4ee8afbb2e4391a67c
Aaron Nolan <N11021276@qut.edu.au>
2022-10-24 22:31:01 +1000

    Update App.js

--- react/client/src/App.js
+++ react/client/src/App.js
@@ -11,7 +11,7 @@ function App() {
       .then((data) => {
         setData(data);
         console.log(data);
-        console.log(data[0][0].username);
+        console.log(data.locations[0].username);
       })
       .catch((e) => {
         console.log(e);
