c2e0985417a8489a237a227980d058b195e5af5d
Aaron Nolan <N11021276@qut.edu.au>
2022-10-24 22:20:39 +1000

    Test #2

--- react/client/src/App.js
+++ react/client/src/App.js
@@ -8,7 +8,10 @@ function App() {
   useEffect(() => {
     fetch("http://ec2-44-212-25-96.compute-1.amazonaws.com:80/locations")
       .then((res) => res.json())
-      .then((data) => setData(data))
+      .then((data) => {
+        setData(data);
+        console.log(data);
+      })
       .catch((e) => {
         console.log(e);
       });
@@ -19,9 +22,17 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          {data[1]}
+          {data[1][1].username}
+          {data[1][2].id}
+          {data[1][3].id}
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <div>
+          {data[1][1].username}
+          {data[1][2].id}
+          {data[1][3].id}
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
