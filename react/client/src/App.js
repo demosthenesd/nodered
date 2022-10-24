997b49854b3f63048324e73bc59a1c3e285a0e37
Aaron Nolan <N11021276@qut.edu.au>
2022-10-25 09:53:29 +1000

    Remove react boilerplate html

--- react/client/src/App.js
+++ react/client/src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { useEffect, useState } from "react";
 
@@ -22,11 +21,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
         <ul>
           {data.map((entry) => (
             <li key={entry.id}>
@@ -34,15 +28,6 @@ function App() {
             </li>
           ))}
         </ul>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
