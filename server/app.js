71e24a466745ab175b777fd34dd20d4734403346
demosthenesd <demosthenesd@icloud.com>
2022-10-24 19:58:47 +1000

    Update app.js

--- server/app.js
+++ server/app.js
@@ -9,16 +9,14 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+var cors = require('cors');
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header("Access-Control-Allow-Methods", "GET");
 
-  if (req.method === "OPTIONS") return res.status(200).json({});
+app.use(cors({
+  origin: '*'
+}));
 
-  next();
-});
+
 
 
 
