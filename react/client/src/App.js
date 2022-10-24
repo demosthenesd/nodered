cd1fe6f63e36fb0a5bb9066f03b10ac9c2e48439
Aaron Nolan <N11021276@qut.edu.au>
2022-10-24 23:00:19 +1000

    Update App.js

--- react/client/src/App.js
+++ react/client/src/App.js
@@ -24,7 +24,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          {data.locations[0].username}
+          {data && data.locations[0].username}
         </p>
 
         <a
