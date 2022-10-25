b0f7a6cb85528044b3f0e192cadfa73c39c2231a
demosthenesd <demosthenesd@icloud.com>
2022-10-25 20:55:54 +1000

    new Patient table postgres

--- server/database/models/init-models.js
+++ server/database/models/init-models.js
@@ -1,12 +1,17 @@
 var DataTypes = require("sequelize").DataTypes;
 var _location = require("./location");
+var _patient = require("./patient");
+
 
 function initModels(sequelize) {
   var location = _location(sequelize, DataTypes);
+  var patient = _patient(sequelize, DataTypes);
+
 
 
   return {
     location,
+    patient
   };
 }
 module.exports = initModels;
