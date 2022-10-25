65fb50ce6787c1b8b7b3efebacacae0e47984965
Aaron Nolan <N11021276@qut.edu.au>
2022-10-25 12:57:37 +1000

    Updated URL

--- react/client/src/App.js
+++ react/client/src/App.js
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 function App() {
   const [data, setData] = useState([]);
 
-  const URL = "http://44.212.25.96/";
+  const URL = "http://18.208.133.236/";
   const ENDPOINT = "locations";
 
   useEffect(() => {
