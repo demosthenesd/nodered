e56e27e0846caaaf9ce3ced27db089575925619e
Aaron Nolan <N11021276@qut.edu.au>
2022-10-25 10:57:58 +1000

    Update EC2 IP

--- react/client/src/App.js
+++ react/client/src/App.js
@@ -5,7 +5,7 @@ import "ag-grid-community/dist/styles/ag-grid.css";
 import "ag-grid-community/dist/styles/ag-theme-balham.css";
 
 function App() {
-  const URL = "http://54.163.22.133/";
+  const URL = "http://44.205.253.206/";
   const ENDPOINT = "locations";
 
   const [rowData, setRowData] = useState([]);
