59b098892c4fa95ce9235423ad35ff5bebce6146
Aaron Nolan <N11021276@qut.edu.au>
2022-10-25 11:20:08 +1000

    Update App.js

--- react/client/src/App.js
+++ react/client/src/App.js
@@ -5,7 +5,7 @@ import "ag-grid-community/dist/styles/ag-grid.css";
 import "ag-grid-community/dist/styles/ag-theme-balham.css";
 
 function App() {
-  const URL = "http://44.205.253.206/";
+  const URL = "http://44.212.1.119/";
   const ENDPOINT = "locations";
 
   const [rowData, setRowData] = useState([]);
